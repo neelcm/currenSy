e73a3023ea2c7f85d3d00fed2f39cf0814f39344
Nikhil Srinivasan <nikhil@nvs.io>
2014-02-15 07:19:39 -0500

    add coinbaseapi class

---
+++ ios/currensy/currensy/CoinbaseAPI.h
@@ -0,0 +1,13 @@
+//
+//  CoinbaseAPI.h
+//  currensy
+//
+//  Created by Nikhil Srinivasan on 2/15/14.
+//  Copyright (c) 2014 bitpass. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoinbaseAPI : NSObject
+
+@end
